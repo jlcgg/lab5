d65b1e9918b4e73b60e43af1a7f1d0aed0202e5d
jlcgg <janete.carramona@gmail.com>
2021-06-03 16:15:35 +0100

    task2

---
+++ task2/src/main/java/pp/ual/pt/App.java
@@ -0,0 +1,59 @@
+package pp.ual.pt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+interface PairAggregator<T> {
+    T aggregate(Pair<T> pair);
+}
+
+class Pair<T> {
+    private final T a;
+    private final T b;
+
+    public Pair(T a, T b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public T getA() { return a; }
+
+    public T getB() { return b; }
+}
+
+class PairCollection<T> {
+    private final List<Pair<T>> pairs = new ArrayList<>();
+
+    void addPair(Pair<T> pair) { pairs.add(pair); }
+
+    List<T> compute(PairAggregator<T> aggregator) {
+        ArrayList<T> result = new ArrayList<>();
+        for(final var pair : pairs) {
+            result.add(aggregator.aggregate(pair));
+        }
+        return result;
+    }
+}
+
+/*class SumPairs implements PairAggregator<Integer> {
+    @Override
+    public Integer aggregate(final Pair<Integer> pair) {
+        return pair.getA() + pair.getB();
+    }
+}*/
+
+public class App {
+    public static void main(String[] args) {
+        PairCollection<Integer> intPairs = new PairCollection<>();
+        intPairs.addPair(new Pair<Integer>(10, 1));
+        intPairs.addPair(new Pair<Integer>(20, 2));
+        intPairs.addPair(new Pair<Integer>(30, 3));
+
+        List<Integer> results = intPairs.compute(pair -> pair.getA() + pair.getB());
+        results.forEach(System.out::println);
+
+        /*for(final var result : results) {
+            System.out.println(result);
+        }*/
+    }
+}
